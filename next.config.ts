c9906b59482a336c5240005c1aed8545aa84c467
TiarSukaCoding <tiarsukacoding@gmail.com>
2025-05-24 19:40:57 +0700

    add deploy scripts to github page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Required for static export
+  basePath: process.env.NODE_ENV === 'production' ? '/Mikrobait' : '', // GitHub Pages subpath
+  images: {
+    unoptimized: true, // Disable Next.js Image Optimization (static export)
+  },
+  // Optional: Add trailingSlash if links break in GitHub Pages
+  trailingSlash: true,
 };
 
 export default nextConfig;
